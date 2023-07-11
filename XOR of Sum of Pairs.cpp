6cdf65204253d0a462ee78ed15b0ed5810a60fb3
Pragna Yanduri <109524200+Pragna235@users.noreply.github.com>
2023-07-11 19:40:10 +0530

    Create XOR of Sum of Pairs.cpp

---
+++ XOR of Sum of Pairs.cpp
@@ -0,0 +1,26 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
+    int t;
+    cin>>t;
+    while(t--){
+        int n,ans=0;
+        cin>>n;
+        int arr[n];
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+        }
+        for(int i=0;i<n;i++){
+            ans^=arr[i]+arr[i];
+        }
+        cout<<ans<<endl;
+    }
+    return 0;
+}
