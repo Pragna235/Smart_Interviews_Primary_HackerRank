01ffc31ac9e634cd06f87be8c8f320402ee63e99
Pragna Yanduri <109524200+Pragna235@users.noreply.github.com>
2023-07-24 19:48:54 +0530

    Create Tower of Hanoi Easy.cpp

---
+++ Tower of Hanoi Easy.cpp
@@ -0,0 +1,42 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int c=0;
+void solve(int n, char src, char dest, char temp){
+   
+
+   
+    //cout<<"Move "<<a<<" from "<<src<<" to "<<dest<<endl;
+        if(n==0)
+    return;
+   
+   
+   
+        solve(n-1,src,temp,dest);
+        cout<<"Move "<<n<<" from "<<src<<" to "<<dest<<endl;
+        solve(n-1,temp,dest,src);
+   
+}
+void solve1(int n, char src, char dest, char temp){
+   
+
+   
+    //cout<<"Move "<<a<<" from "<<src<<" to "<<dest<<endl;
+        if(n==0)
+    return ;
+   
+    //c can be calculated using (2**n)-1
+   
+        solve1(n-1,src,temp,dest);
+       
+        solve1(n-1,temp,dest,src);
+    c++;
+   
+}
+
+
+
