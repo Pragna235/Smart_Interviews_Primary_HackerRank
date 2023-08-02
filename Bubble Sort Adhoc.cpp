7f478686472ea32bb10d7bd7141be13143a10bcf
Pragna Yanduri <109524200+Pragna235@users.noreply.github.com>
2023-08-02 19:30:45 +0530

    Create Bubble Sort Adhoc.cpp

---
+++ Bubble Sort Adhoc.cpp
@@ -0,0 +1,33 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int arr[n];
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+        }
+        int cnt=0;
+        for(int i=0;i<n-1;i++){
+            for(int j=0;j<n-i-1;j++){
+                if(arr[j]>arr[j+1]){
+                    swap(arr[j],arr[j+1]);
+                    cnt++;
+                }
+            }
+           
+        }
+        cout<<cnt<<endl;
+    }
+    return 0;
+}
