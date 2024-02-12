b5ba9310cf20672081527d91fba021b69e279e00
Pragna Yanduri <109524200+Pragna235@users.noreply.github.com>
2024-02-12 16:39:22 +0530

    Create Kth Smallest Element.cpp

---
+++ Kth Smallest Element.cpp
@@ -0,0 +1,24 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int t;
+    cin>>t;
+    while(t--){
+        int n,k;
+        cin>>n>>k;
+        int arr[n];
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+        }
+        sort(arr,arr+n);
+        cout<<arr[k-1]<<endl;
+    }
+    return 0;
+}
